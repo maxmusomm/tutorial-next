07a7cf3d8ba0c0ab5e73aa2c6303721ce5e5cede
maxmusomm <maxmusomm@gmail.com>
2025-04-20 18:55:01 +0200

    tried fixes

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -144,7 +144,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
